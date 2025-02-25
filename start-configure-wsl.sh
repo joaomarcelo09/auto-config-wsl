21047fdd5f15e72c067240780465e7730d03c7df
Joao Gomes <jg0112360@gmail.com>
2025-02-25 01:50:09 -0300

    fix: change order instalation

--- start-configure-wsl.sh
+++ start-configure-wsl.sh
@@ -8,7 +8,6 @@ chmod +x ./setup/*.sh
 echo "Iniciando o setup..."
 
 ./setup/install-zsh.sh
-./setup/install-oh-my-zsh.sh
 ./setup/install-zsh-plugins.sh
 ./setup/install-nvm.sh
 ./setup/configure-github.sh
@@ -18,5 +17,9 @@ echo "Iniciando o setup..."
 # Rodar os testes após o setup
 echo "Iniciando os testes..."
 ./setup/run-tests.sh
+
+# Instala a parte visual do zsh
+echo "Instalando aparencia do shell"
+./setup/install-oh-my-zsh.sh
 
 echo "Setup concluído!"
